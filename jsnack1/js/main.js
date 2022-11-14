fdb41be444e38ae69e2116d826ecd90acb16c457
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-14 13:54:30 +0100

    Finito il primo JSnack con la soluzione più veloce disponibile

---
+++ jsnack1/js/main.js
@@ -0,0 +1,8 @@
+let num1 = parseFloat(prompt("Inserisci un numero: "));
+let num2 = parseFloat(prompt("Inserisci un'altro numero: "));
+
+let numeroPiuGrande;
+numeroPiuGrande = num1 > num2 ? num1 : num2;
+
+
+console.log(numeroPiuGrande);
