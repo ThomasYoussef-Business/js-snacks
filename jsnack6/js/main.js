2f71d497829bc2127afe23213e0849dcf329f821
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-14 17:23:22 +0100

    Finito JSnack 6

--- jsnack6/js/main.js
+++ jsnack6/js/main.js
@@ -1,33 +1,18 @@
 "use strict";
-/*
-Per arrivare da un numero all'altro, possiamo usare Math.random come una percentuale
-Quindi se numeroCasuale (o d per Delta) è 0.5,
-il nostro numero più piccolo è 1, o l per Lower Bound,
-mentre il nostro numero più grande è 10, o u per Upper Bound,
-la nostra equazione diventa
 
-(u - l)             * d                 + l
-^                ^                   ^
-Prendi la       Calcola la        Riaggiungi la
-differenza      posizione con     base
-la percentuale
-
-d(u - l) + l
-è la nostra formula finale
-*/
-function lerp(l, u, d) {
-    return d * (u - l) + l;
+function lerp(a, b, delta) {
+    return (a * (1 - delta)) + (b * delta);
 }
 
 let numeroMassimo = parseInt(prompt("Inserisci un numero massimo da generare: "));
-let numeroCasuale = Math.random()
+let numeroCasuale = Math.random();
 
-for (let i = 0; i < 500; i++) {
-    const rngl = Math.random() * 100;
-    const rngu = Math.random() * 100;
-    const rngd = 0.01;
-    console.log(`Lower Bound: ${rngl}
-    Upper Bound: ${rngu}
-    Delta: ${rngd}
-    Random Number: ${Math.round(lerp(rngl, rngu, rngd))}`);
+numeroCasuale = Math.round(lerp(1, numeroMassimo, numeroCasuale));
+
+let numeroScelto = parseInt(prompt("Scegli il tuo numero: "));
+
+if (numeroScelto == numeroCasuale) {
+    console.log("Hai vinto!");
+} else {
+    console.log(`Hai perso... il numero era ${numeroCasuale}`);
 }
